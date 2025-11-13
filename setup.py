adc22628474f0210d92968b1164b4a3112e816fb
Michal Treter <mtreter@outlook.com>
2025-11-13 18:19:23 +0100

    Fix formatting

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ from setuptools import setup, find_packages
 
 # Read the contents of README file
 from pathlib import Path
+
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text(encoding='utf-8')
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="string-randomizer",
